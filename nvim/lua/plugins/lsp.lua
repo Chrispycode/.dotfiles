0c31782988d6e95710cf0ddd80b9e26bd14e92ec
Christopher <>
2024-09-05 15:17:54 +0200

    emmet lsp to run only in root

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -79,6 +79,11 @@ return {
           return vim.loop.cwd()
         end,
       },
+      emmet_language_server = {
+        root_dir = function()
+          return vim.loop.cwd()
+        end,
+      },
       html = {
         filetypes = { 'html', 'eruby' },
       },
