ab32ce0873071262a509f56de72f96192aabc183
chrispycode <you@example.com>
2024-09-30 00:08:13 +0200

    term in nvim

---
+++ nvim/lua/plugins/clis.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    dependencies = {
+      { 'tpope/vim-dadbod', lazy = true },
+      { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+      'DBUI',
+      'DBUIToggle',
+      'DBUIAddConnection',
+      'DBUIFindBuffer',
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.api.nvim_set_keymap('n', '<leader>td', ':DBUI<CR>', { noremap = true, silent = true })
+    end,
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = function()
+      require('toggleterm').setup {
+        direction = 'float',
+        float_opts = {
+          border = 'double',
+        },
+      }
+      local Terminal = require('toggleterm.terminal').Terminal
+      local lazydocker = Terminal:new { cmd = 'lazydocker', hidden = true }
+
+      function _lazydocker_toggle()
+        lazydocker:toggle()
+      end
+      vim.api.nvim_set_keymap('n', '<leader>tt', ':ToggleTerm<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>ld', '<cmd>lua _lazydocker_toggle()<CR>', { noremap = true, silent = true })
+    end,
+  },
+}
