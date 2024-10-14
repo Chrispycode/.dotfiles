d534388c56251b75224446e51ea8b7d9b7fb4531
Christopher <>
2024-10-14 15:49:52 +0200

    ignore .bundle folder

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -52,6 +52,7 @@ return {
           file_ignore_patterns = {
             'node_modules',
             'tmp',
+            '.bundle',
             '.idea',
             '.loadpath',
             '.powrc',
