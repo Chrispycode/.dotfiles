e825059ff82b3870cad1fe4ba6bf4bc21a79f995
chrispycode <you@example.com>
2025-03-25 21:40:16 +0100

    upgrade cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,7 +2,7 @@ return {
   {
     'saghen/blink.cmp',
     dependencies = 'rafamadriz/friendly-snippets',
-    version = 'v0.*',
+    version = 'v1.*',
     opts = {
       keymap = {
         preset = 'default',
