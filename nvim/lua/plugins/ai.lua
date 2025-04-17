f50c7ada3f8c362bdc00050373d9815dc942534b
christopher <you@example.com>
2025-04-17 10:27:41 +0200

    change copilot default model

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -53,7 +53,8 @@ return {
           return require('codecompanion.adapters').extend('copilot', {
             schema = {
               model = {
-                default = 'claude-3.5-sonnet',
+                -- default = 'claude-3.5-sonnet',
+                default = 'gpt-4.1',
               },
               temperature = { default = 0.6 },
             },
