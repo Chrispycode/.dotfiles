341904a8a532e4190f9c94f284f29ef69ac07e0b
chrispycode <you@example.com>
2025-05-10 15:58:29 +0200

    udpate mason

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-		-- used for completion, annotations and signatures of Neovim apis
+		-- used for completion, annotations and signatures of Neovim APIs
 		'folke/lazydev.nvim',
 		ft = 'lua',
 		opts = {
@@ -14,10 +14,8 @@ return {
 	{
 		'neovim/nvim-lspconfig',
 		dependencies = {
-			-- { 'mason-org/mason.nvim', config = true },
-			-- 'mason-org/mason-lspconfig.nvim',
-			{ "mason-org/mason.nvim",           version = "1.11.0", config = true },
-			{ "mason-org/mason-lspconfig.nvim", version = "1.32.0" },
+			{ 'williamboman/mason.nvim', config = true },
+			{ 'williamboman/mason-lspconfig.nvim' },
 			'WhoIsSethDaniel/mason-tool-installer.nvim',
 			'saghen/blink.cmp',
 		},
@@ -29,17 +27,11 @@ return {
 					local map = function(keys, func, desc)
 						vim.keymap.set('n', keys, func, { desc = 'LSP: ' .. desc })
 					end
-					-- local fzf_lua = require 'fzf-lua'
-					-- map('gd', fzf_lua.lsp_definitions, '[G]oto [D]efinition')
-					-- map('gr', fzf_lua.lsp_references, '[G]oto [R]eferences')
-					-- map('gI', fzf_lua.lsp_implementations, '[G]oto [I]mplementation')
-					-- map('<leader>D', fzf_lua.lsp_typedefs, 'Type [D]efinition')
-					-- map('<leader>ds', fzf_lua.lsp_document_symbols, '[D]ocument [S]ymbols')
-					-- map('<leader>ws', fzf_lua.lsp_live_workspace_symbols, '[W]orkspace [S]ymbols')
 					map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 					map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 					map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 					map('<leader>ld', vim.diagnostic.open_float, '[l]ine [D]iagnostic')
+
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
 						local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
@@ -48,7 +40,6 @@ return {
 							group = highlight_augroup,
 							callback = vim.lsp.buf.document_highlight,
 						})
-						-- vim.api.nvim_buf_set_l
 						vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
 							buffer = event.buf,
 							group = highlight_augroup,
@@ -104,18 +95,17 @@ return {
 				'rufo',
 				'prettier'
 			})
-			require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
+			require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 			require('mason-lspconfig').setup {
-				handlers = {
-					function(server_name)
-						local server = servers[server_name] or {}
-						local capabilities = require('blink.cmp').get_lsp_capabilities()
-						server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-						require('lspconfig')[server_name].setup(server)
-					end,
-				},
+				automatic_installation = true,
 			}
+
+			for server_name, server_config in pairs(servers) do
+				local capabilities = require('blink.cmp').get_lsp_capabilities()
+				server_config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server_config.capabilities or {})
+				require('lspconfig')[server_name].setup(server_config)
+			end
 		end,
 	},
 	{
