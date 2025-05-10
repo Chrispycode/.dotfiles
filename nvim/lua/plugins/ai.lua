6b5839e305ae8678d1da6d72491e310663fd78d0
chrispycode <you@example.com>
2025-05-10 20:51:22 +0200

    lsp changes

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -1,66 +1,66 @@
 return {
-  {
-    'github/copilot.vim',
-    config = function()
-      vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<CR>")', {
-        expr = true,
-        replace_keycodes = false
-      })
-      vim.g.copilot_no_tab_map = true
-    end
-  },
-  {
-    'olimorris/codecompanion.nvim',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      { "nvim-lua/plenary.nvim",  branch = "master" },
-      { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
-    },
-    opts = {
-      display = {
-        action_palette = {
-          provider = "default"
-        },
-      },
-      strategies = {
-        chat = {
-          adapter = os.getenv 'LLM',
-        },
-        inline = {
-          adapter = os.getenv 'LLM',
-        },
-      },
-      adapters = {
-        ollama = function()
-          return require('codecompanion.adapters').extend('ollama', {
-            env = {
-              url = os.getenv 'LLM_URL',
-              api_key = os.getenv 'LLM_API_KEY',
-            },
-            schema = {
-              num_ctx = {
-                default = 64000,
-              },
-              model = {
-                default = 'llama3.1:latest',
-                -- default = 'deepseek-r1:14b',
-              },
-              temperature = { default = 0.6 },
-            },
-          })
-        end,
-        copilot = function()
-          return require('codecompanion.adapters').extend('copilot', {
-            schema = {
-              model = {
-                -- default = 'claude-3.5-sonnet',
-                default = 'gpt-4.1',
-              },
-              temperature = { default = 0.6 },
-            },
-          })
-        end,
-      },
-    }
-  },
+	{
+		'github/copilot.vim',
+		config = function()
+			vim.keymap.set('i', '<S-Tab>', 'copilot#Accept("\\<CR>")', {
+				expr = true,
+				replace_keycodes = false
+			})
+			vim.g.copilot_no_tab_map = true
+		end
+	},
+	{
+		'olimorris/codecompanion.nvim',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter',
+			{ "nvim-lua/plenary.nvim",  branch = "master" },
+			{ 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
+		},
+		opts = {
+			display = {
+				action_palette = {
+					provider = "default"
+				},
+			},
+			strategies = {
+				chat = {
+					adapter = os.getenv 'LLM',
+				},
+				inline = {
+					adapter = os.getenv 'LLM',
+				},
+			},
+			adapters = {
+				ollama = function()
+					return require('codecompanion.adapters').extend('ollama', {
+						env = {
+							url = os.getenv 'LLM_URL',
+							api_key = os.getenv 'LLM_API_KEY',
+						},
+						schema = {
+							num_ctx = {
+								default = 64000,
+							},
+							model = {
+								default = 'llama3.1:latest',
+								-- default = 'deepseek-r1:14b',
+							},
+							temperature = { default = 0.6 },
+						},
+					})
+				end,
+				copilot = function()
+					return require('codecompanion.adapters').extend('copilot', {
+						schema = {
+							model = {
+								-- default = 'claude-3.5-sonnet',
+								default = 'gpt-4.1',
+							},
+							temperature = { default = 0.6 },
+						},
+					})
+				end,
+			},
+		}
+	},
 }
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,22 +1,21 @@
 return {
 	{
 		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-		-- used for completion, annotations and signatures of Neovim APIs
-		'folke/lazydev.nvim',
-		ft = 'lua',
-		opts = {
-			library = {
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = 'luvit-meta/library', words = { 'vim%.uv' } },
-			},
-		},
+		-- used for completion, annotations and signatures of Neovim apis
+    'folke/lazydev.nvim',
+    ft = 'lua',
+    opts = {
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    }
 	},
 	{
 		'neovim/nvim-lspconfig',
 		dependencies = {
-			{ 'williamboman/mason.nvim', config = true },
+			{ 'williamboman/mason.nvim',          config = true },
 			{ 'williamboman/mason-lspconfig.nvim' },
-			'WhoIsSethDaniel/mason-tool-installer.nvim',
 			'saghen/blink.cmp',
 		},
 		config = function()
@@ -32,14 +31,23 @@ return {
 					map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 					map('<leader>ld', vim.diagnostic.open_float, '[l]ine [D]iagnostic')
 
+					local function client_supports_method(client, method, bufnr)
+						if vim.fn.has 'nvim-0.11' == 1 then
+							return client:supports_method(method, bufnr)
+						else
+							return client.supports_method(method, { bufnr = bufnr })
+						end
+					end
+
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+					if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
 						local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
 						vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
 							buffer = event.buf,
 							group = highlight_augroup,
 							callback = vim.lsp.buf.document_highlight,
 						})
+
 						vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
 							buffer = event.buf,
 							group = highlight_augroup,
@@ -54,8 +62,7 @@ return {
 							end,
 						})
 					end
-
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+					if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
 						map('<leader>th', function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
 						end, '[T]oggle Inlay [H]ints')
@@ -96,9 +103,10 @@ return {
 				'prettier'
 			})
 
-			require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 			require('mason-lspconfig').setup {
 				automatic_installation = true,
+				automatic_enable = true,
+				ensure_installed = vim.tbl_keys(servers)
 			}
 
 			for server_name, server_config in pairs(servers) do
