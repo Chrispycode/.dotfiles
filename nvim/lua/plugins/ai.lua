72832268180f9db95dd2f9182ae67444312ce35a
chrispycode <you@example.com>
2025-06-03 00:16:24 +0200

    fix keymap

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -3,6 +3,7 @@ return {
 		'zbirenbaum/copilot.lua',
 		cmd = 'Copilot',
 		config = function()
+    	vim.keymap.del('i', '<S-Tab>')
 			require('copilot').setup({
 				suggestion = {
 					enabled = true,
