e947f804b208089d21b5aa9295601c067d58a073
christopher <you@example.com>
2025-06-12 00:00:41 +0200

    add weird aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -5,7 +5,12 @@ alias s="ssh"
 alias n="nvim"
 alias lg="lazygit"
 alias icat="kitty +kitten icat"
-dca() { dcup -d $@ && docker attach $@ }
+dua() { dcup -d $@ && docker attach $@ }
+dup() { dcup -d $dcup -d $@ &&{PWD##*/} }
+dupf() { dcup -d --force-recreate ${PWD##*/} }
+dupa() { dcup -d ${PWD##*/} && docker attach ${PWD##*/} }
+ds() { dcr --rm ${PWD##*/} }
+dsb() { dcr --rm ${PWD##*/} bash }
 alias dev_start="sudo systemctl start ollama.service" #" docker.service docker.socket"
 alias dev_stop="sudo systemctl stop ollama.service" #" docker.service docker.socket containerd.service"
 
