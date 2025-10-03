325d0239c29601caa229ca2cad7f4fba68fa6f19
chrispycode <you@example.com>
2025-10-03 13:18:34 +0200

    blubb

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.opt.number = true
 vim.opt.mouse = 'a'
 vim.opt.showmode = false
@@ -28,8 +30,6 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.relativenumber = true
 vim.opt.laststatus = 3
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 vim.opt.spell = true
 vim.opt.spelllang = { 'en', 'de' }
 vim.opt.wrap = false
@@ -37,10 +37,10 @@ vim.opt.wrapmargin = 10
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldlevelstart = 1
-vim.api.nvim_set_hl(0, 'Normal', { bg = 0 })
-vim.api.nvim_set_hl(0, 'NonText', { bg = 0 })
 vim.filetype.add { pattern = { ['.*%.api%.rsb'] = 'ruby' } }
 vim.filetype.add { pattern = { ['.*%.yml%.j2'] = 'yaml' } }
+vim.api.nvim_set_hl(0, 'Normal', { bg = 0 })
+vim.api.nvim_set_hl(0, 'NonText', { bg = 0 })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = { "*.slim*" }, command = "set ft=slim" })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" },	{ pattern = { "*_patch.rb", "*_hook.rb" }, command = "setlocal foldlevel=4" })
 
